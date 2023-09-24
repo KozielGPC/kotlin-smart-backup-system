6e2b338cd221a59052d5171f020d2e2e5e6d4a8d
KozielGPC <gpcgabriel0@gmail.com>
2023-09-24 20:12:43 -0300

    create function to check if file was modified

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.attribute.FileTime
 
 fun initBackupDirectory(destinationDir: String) {
     val destinationPath = Paths.get(destinationDir)
@@ -13,6 +15,28 @@ fun initBackupDirectory(destinationDir: String) {
             println("Erro ao criar pasta de destino: ${e.message}")
             return
         }
+    }
+}
+
+fun isFileModified(sourcePath: String, destinationPath: String): Boolean {
+    val sourceFile = Paths.get(sourcePath)
+    val destinationFile = Paths.get(destinationPath)
+
+    if (!Files.exists(destinationFile)) {
+        return true // Se o arquivo de destino não existir, ele foi modificado
+    }
+
+    try {
+        val sourceAttributes = Files.readAttributes(sourceFile, BasicFileAttributes::class.java)
+        val destinationAttributes = Files.readAttributes(destinationFile, BasicFileAttributes::class.java)
+
+        val sourceModifiedTime = sourceAttributes.lastModifiedTime()
+        val destinationModifiedTime = destinationAttributes.lastModifiedTime()
+
+        return sourceModifiedTime > destinationModifiedTime
+    } catch (e: IOException) {
+        println("Erro ao verificar se o arquivo foi modificado: ${e.message}")
+        return false // Assumir que houve uma falha na verificação e tratar como não modificado
     }
 }
 
@@ -30,6 +54,8 @@ fun writeFileToBackup(sourceFilePath: String, destinationDir: String) {
     }
 }
 
+
+
 fun main(args: Array<String>) {
     val sourceDirectory = "files-to-backup"
     val destinationDirectory = "backup-folder"
@@ -37,4 +63,13 @@ fun main(args: Array<String>) {
     initBackupDirectory(destinationDirectory)
 
     writeFileToBackup("files-to-backup/texto1.txt", destinationDirectory)
+
+    val sourceFilePath = "files-to-backup/texto1.txt"
+    val destinationFilePath = "backup-folder/texto1.txt"
+
+    if (isFileModified(sourceFilePath, destinationFilePath)) {
+        println("O arquivo foi modificado e precisa ser copiado para o destino.")
+    } else {
+        println("O arquivo não foi modificado e não precisa ser copiado para o destino.")
+    }
 }
